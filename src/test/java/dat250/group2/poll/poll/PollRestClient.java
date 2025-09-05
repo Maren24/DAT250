021d6ae3fff5ceeb2f38754dbe105c7c578be0d2
KamilMatyjaszczyk <Kamilmaty@hotmail.com>
2025-09-05 17:38:55 +0200

    Forgot to import comparator

--- src/test/java/dat250/group2/poll/poll/PollRestClient.java
+++ src/test/java/dat250/group2/poll/poll/PollRestClient.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestClient;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Comparator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
